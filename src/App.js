acaad041e334cb0f887519dc76c99d224dc69916
AlsonZ <48876218+AlsonZ@users.noreply.github.com>
2020-10-23 21:52:35 +1100

    create decrement button to satisfy tests

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
       <h2>TDD Counter</h2>
       <div id="counter-value">{counter}</div>
       <button id="increment-btn" onClick={()=>{setCounter(counter+1)}}>Increment</button>
+      <button id="decrement-btn" onClick={()=>{setCounter(counter-1)}}>Decrement</button>
     </div>
   );
 }
